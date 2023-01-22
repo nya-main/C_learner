56049b459a60bfd898c8a261cb770f240d9a0a4f
nnyyaa <hongpengbo@gmail.com>
2023-01-22 10:11:54 +0800

    i

---
+++ 2023-1-21_3/test.c
@@ -0,0 +1,13 @@
+//shit code//
+#include <stdio.h>
+
+int int main(void)
+{
+  char A = "a";
+  char B = "b";
+  printf("char A(a): %c\n", A);
+  printf("int A(a): %d\n", A);
+  printf("char B(114514): %c\n", B);
+  printf("int B(114514): %d\n", B);
+  return 0;
+}
