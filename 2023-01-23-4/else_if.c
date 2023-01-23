04dc6ee1f4334accfb460d09b4a030d43014e861
nnyyaa <hongpengbo@gmail.com>
2023-01-23 13:27:53 +0800

    更新

---
+++ 2023-01-23-4/else_if.c
@@ -0,0 +1,18 @@
+//shitcode
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+int main(void)
+{
+    int number = 0;
+    printf("输入一个在%d到%d的数字: ", INT_MIN, INT_MAX);
+    scanf("%d", &number);
+    if(number > 10)
+        printf("你输入的是一个正数\n");
+    else if(number == 0)
+        printf("你输入的是0\n");
+    else if(number < 0)
+        printf("你输入的是一个负数\n");
+    return 0;
+}
