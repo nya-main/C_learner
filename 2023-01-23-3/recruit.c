513ffa6fbdd5625ce9425653ab02af2e585db103
nnyyaa <hongpengbo@gmail.com>
2023-01-23 13:06:48 +0800

    更新

--- 2023-01-23-3/recruit.c
+++ 2023-01-23-3/recruit.c
@@ -12,11 +12,24 @@ int main(void)
     int age = 0; //年龄
     int school = 0; //学校
     int subject = 0; //学科
-    bool is_reject = false; //布尔拒绝测试
+    bool result = false; //布尔拒绝测试
     printf("输入你的年龄: ");
     scanf("%d", &age);
     printf("输入你的学校(\"1\"代表哈佛大学,\"2\"代表耶鲁大学,\"3\"代表其他大学): ");
     scanf("%d", &school);
-    printf("输入你的学科(\"1\"代表化学专业,\"2\"代表经济专业,\"3\"代表其他专业): ");
+    printf("输入你的学科(\"1\"代表化学专业,\"2\"代表经济专业): ");
     scanf("%d", &subject);
+    if(age > 25 && subject == 1 && school != 2 )
+        result = true;
+    if(subject == 1 && school == 2)
+        result = true;
+    if(age > 25 && school == 2 && subject != 1)
+        result = true;
+    if(age < 28 && school == 1 && subject == 2)
+        result = true;
+    if(result)
+        printf("你被录取了!\n");
+    else
+        printf("你没有被录取哦...\n");
+    return 0;
 }
